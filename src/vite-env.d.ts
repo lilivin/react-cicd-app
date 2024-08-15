e693b9c36e061dac5d27fcda34744c08bbe0afc2
kliwinski <kliwinski@deloittece.com>
2024-08-15 12:34:30 +0200

    New

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,12 @@
 /// <reference types="vite/client" />
 
-// Rozszerzenie obiektu import.meta.env przez nową zmienną środowiskową
 interface ImportMetaEnv {
     readonly VITE_ENVIRONMENT: string;
-  }
+}
   
-  interface ImportMeta {
+interface ImportMeta {
     readonly env: ImportMetaEnv;
-  }
+}
   
-  // Zmienna globalna wystawiona przez konfigurację "define"
-  declare const __APP_VERSION__: string;
+// Zmienna globalna wystawiona przez konfigurację "define"
+declare const __APP_VERSION__: string;
