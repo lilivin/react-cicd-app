7d60ab36c128ba9111f25ca47cb4dd0842584ce9
kliwinski <kliwinski@deloittece.com>
2024-08-15 12:31:48 +0200

    Added

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import packageJson from "../package.json";
 
 function App() {
   const [count, setCount] = useState(0)
+  const environment = import.meta.env.VITE_ENVIRONMENT;
 
   return (
     <>
@@ -27,6 +29,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p className="read-the-docs">
+        Wersja: {packageJson.version}
+        <strong>Environment:</strong> {environment}
       </p>
     </>
   )
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+// Rozszerzenie obiektu import.meta.env przez nową zmienną środowiskową
+interface ImportMetaEnv {
+    readonly VITE_ENVIRONMENT: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
+  // Zmienna globalna wystawiona przez konfigurację "define"
+  declare const __APP_VERSION__: string;
