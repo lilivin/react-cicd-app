eb6cc3e7b50a5ab65f0b05a5ffa980c8322ef985
Karol Liwiński <k.liwinski@gmail.com>
2024-08-17 21:19:41 +0200

    Finish aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,14 @@ terraform {
   }
 
   required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "react-cicd-state-bucket-test"
+    key            = "state/terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
 }
 
 provider "aws" {
